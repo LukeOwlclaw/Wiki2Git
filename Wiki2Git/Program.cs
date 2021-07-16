1db24a9c70bc185aae8f0875ecbb6875a90b3943
LukeOwlclaw <2737900+LukeOwlclaw@users.noreply.github.com>
2021-07-16 07:19:40 +0200

    Initial commit

---
+++ Wiki2Git/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+// Ignore spelling: Wikimedia Wikipedia
+namespace Wiki2Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Download history of Wikimedia article via https://en.wikipedia.org/wiki/Special:Export
+            // Create C# classes from XML: https://docs.microsoft.com/en-us/dotnet/standard/serialization/xml-schema-definition-tool-xsd-exe
+            // > xsd.exe Wikipedia-20210716042420.xml
+            // > xsd.exe Wikipedia-20210716042420.xsd /classes
+            //var parser = new WikiParser();
+            //var ser = new System.Xml.Serialization.XmlSerializer(typeof(mediawiki));
+            var fs = new FileStream(@"c:\devel\Wiki2Git\Wikipedia-20210716042420.xml", FileMode.Open);
+            var serializer = new Serializer();
+            var wiki = serializer.Deserialize<mediawiki>(fs);
+            foreach (var item in wiki.page)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Wiki2Git/Serializer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Wiki2Git
+{
+    /// <summary>
+    /// https://www.codeproject.com/Articles/1163664/Convert-XML-to-Csharp-Object
+    /// </summary>
+    public class Serializer
+    {
+        public T Deserialize<T>(string input) where T : class
+        {
+            System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T));
+
+            using (StringReader sr = new StringReader(input))
+            {
+                return (T)ser.Deserialize(sr);
+            }
+        }
+
+        public T Deserialize<T>(Stream input) where T : class
+        {
+            System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T));
+
+            return (T)ser.Deserialize(input);
+        }
+
+        public string Serialize<T>(T ObjectToSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(ObjectToSerialize.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, ObjectToSerialize);
+                return textWriter.ToString();
+            }
+        }
+    }
+}
